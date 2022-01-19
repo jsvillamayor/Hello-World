1fa708b6c4b5eafa1dbebe3c1871bdb26e8f2745
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-01-19 12:19:24 -0800

    Add files via upload

---
+++ MyFirstCppProgram.cpp
@@ -0,0 +1,16 @@
+/*
+ * MyFirstCppProgram.cpp
+ *
+ *  Created on: Jan 19, 2022
+ *      Author: jamesvillamayor
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello c++ world w/ James" << endl;
+}
+
+
+
